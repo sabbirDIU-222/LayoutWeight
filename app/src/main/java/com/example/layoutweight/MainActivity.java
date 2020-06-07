aaec8a22440ec446cafb7beebb7823d55665cfee
sabbirDIU-222 <islamsaimun1913@hotmail.com>
2020-06-07 17:48:29 +0600

    layout weight is in linealayput

---
+++ app/src/main/java/com/example/layoutweight/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.layoutweight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+// For Understanding the linearlayout weihtSum and layout_weight property
+// we don't put any extra code in java program
